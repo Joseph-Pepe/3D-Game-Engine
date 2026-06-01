b8fea8445a930ab7e32a7991e0023373aae47c80
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2026-06-01 18:44:38 -0400

    Update JobSystem.h

--- JobSystem.h
+++ JobSystem.h
@@ -19,7 +19,6 @@
 // COROUTINE GLOBAL JOB SYSTEM QUEUE (Zero-OS Allocation)
 // ==================================================================================
 
-
 // --- GLOBAL JOB SYSTEM QUEUE --- 
 // Forward declaration so our tasks can see the global queue
 class JobSystem;
