0bc056588b4f757f43ddf7671be99ba7422fb88c
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2026-06-02 09:55:53 -0400

    Create EngineSettings.cpp

---
+++ EngineSettings.cpp
@@ -0,0 +1,5 @@
+#include "EngineSettings.h"
+
+// -- Global instance -- 
+// This is where the memory is actually allocated in your engine's binary (i.e., ensures it is compiled into exactly one translation unit).
+EngineSettings g_EngineSettings;
