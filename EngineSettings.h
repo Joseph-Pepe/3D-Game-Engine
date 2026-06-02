0351db10db67ab0a5af7bc0e2bb7ea661fef488c
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2026-06-02 09:37:48 -0400

    Create EngineSettings.h

---
+++ EngineSettings.h
@@ -0,0 +1,22 @@
+// --- JAVA SYSTEM (CPU Control, Cache Locality, Dependency Management) ---
+struct EngineSettings {
+    int activeParticles = 100000;
+    float tangentialSpeed = 8.0f;
+    float gravityStrength = 5.0f;
+    bool triggerRespawn = false; // Flag to reset the galaxy
+
+    // --- Dimension Toggle ---
+    bool is2DMode = true; 
+    bool modeChanged = false; // Flag to tell the engine to squash/expand the Z-axis
+
+    // --- Hardware Legacy Architectures ---
+    bool isLegacyCPU = false; 
+};
+
+// ==========================================================================
+// EXTERN: Tells every file that includes this header that 'g_EngineSettings' 
+// ==========================================================================
+// exists, but prevents them from allocating their own duplicate copy of it.
+
+// Global instance
+extern EngineSettings g_EngineSettings;
