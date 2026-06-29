bfe6717849354cb73c49f62f7620b98d30aab285
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2026-06-29 11:45:05 -0400

    Update JobSystem.h

--- JobSystem.h
+++ JobSystem.h
@@ -34,7 +34,7 @@ extern "C" void* SwapContext(void** current_rsp, void* target_rsp);
 // The Job System needs the fast PRNG from Math.h for the work-stealing logic!
 #include "Math.h"
 #include "FixedFunction.h"
-#include "RandGenMath.h"
+#include "RandGenMap.h"
 
 // ==================================================================================
 // CROSS-PLATFORM CACHE LINE ALIGNMENT
