f4a794f8d5c2544472a9bfc3c543b7a861823c80
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2026-07-17 12:16:10 -0400

    Update Hardware.h

--- Hardware.h
+++ Hardware.h
@@ -5,7 +5,7 @@
 
 #include "SIMD/SIMDCustomWrapper.h" 
 #include "ParticleSystem.h"
-#include "JobSystem.h"
+#include "FiberjobSystem/JobSystem.h"
 
 // --- COMPILER INTRINSICS FOR CPUID (x86_64 ONLY) ---
 #if defined(__x86_64__) || defined(_M_X64) || defined(__i386__) || defined(_M_IX86)
